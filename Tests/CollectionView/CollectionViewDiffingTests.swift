82ce164f1dad09beab990a1ea655891232f7d480
Benji Encz <Benjamin.Encz@gmail.com>
2018-01-25 10:02:52 -0800

    [Tests] Add CollectionView Diffing Tests
    Adds counterpart of TableView diffing tests for CollectionView. Again, a lot of redundancy that can be removed in future.

---
+++ Tests/CollectionView/CollectionViewDiffingTests.swift
@@ -0,0 +1,153 @@
+//
+//  PlanGrid
+//  https://www.plangrid.com
+//  https://medium.com/plangrid-technology
+//
+//  Documentation
+//  https://plangrid.github.io/ReactiveLists
+//
+//  GitHub
+//  https://github.com/plangrid/ReactiveLists
+//
+//  License
+//  Copyright © 2018-present PlanGrid, Inc.
+//  Released under an MIT license: https://opensource.org/licenses/MIT
+// 
+
+@testable import ReactiveLists
+import XCTest
+
+final class CollectionViewDiffingTests: XCTestCase {
+
+    var collectionViewDataSource: CollectionViewDataSource!
+    var mockCollectionView: TestCollectionView!
+
+    override func setUp() {
+        super.setUp()
+        self.mockCollectionView = TestCollectionView(
+            frame: .zero,
+            collectionViewLayout: UICollectionViewFlowLayout()
+        )
+        self.collectionViewDataSource = CollectionViewDataSource(
+            collectionView: self.mockCollectionView,
+            automaticDiffingEnabled: true
+        )
+    }
+
+    /// Tests that changes to individual rows result in the correct calls to update the
+    /// collection view.
+    ///
+    /// - Note: We're only testing one type of row update since this is sufficient to test the
+    ///   communication between the diffing lib and the collection view. The diffing lib itself has
+    ///   extensive tests for the various diffing scenarios.
+    func testChangingRows() {
+        let initialModel = CollectionViewModel(
+            sectionModels: [
+                CollectionViewModel.SectionModel(
+                    cellViewModels: [],
+                    headerHeight: 44,
+                    footerHeight: 44,
+                    diffingKey: "1"
+                )
+            ]
+        )
+
+        self.collectionViewDataSource.collectionViewModel = initialModel
+
+        let updatedModel = CollectionViewModel(
+            sectionModels: [
+                CollectionViewModel.SectionModel(
+                    cellViewModels: [CollectionUserCellModel(user: User(name: "Mona"))],
+                    headerHeight: 44,
+                    footerHeight: 44,
+                    diffingKey: "1"
+                )
+            ]
+        )
+
+        self.collectionViewDataSource.collectionViewModel = updatedModel
+
+        XCTAssertEqual(self.mockCollectionView.callsToInsertItems.count, 1)
+        XCTAssertEqual(
+            self.mockCollectionView.callsToInsertItems[0],
+            [IndexPath(row: 0, section: 0)]
+        )
+    }
+
+    /// Tests that changes to individual sections result in the correct calls to update the
+    /// collection view.
+    ///
+    /// - Note: We're only testing one type of section update since this is sufficient to test the
+    ///   communication between the diffing lib and the collection view. The diffing lib itself has
+    ///   extensive tests for the various diffing scenarios.
+    func testChangingSections() {
+        let initialModel = CollectionViewModel(
+            sectionModels: [
+                CollectionViewModel.SectionModel(
+                    cellViewModels: [],
+                    headerHeight: 44,
+                    footerHeight: 44,
+                    diffingKey: "1"
+                ),
+                CollectionViewModel.SectionModel(
+                    cellViewModels: [],
+                    headerHeight: 44,
+                    footerHeight: 44,
+                    diffingKey: "2"
+                )
+            ]
+        )
+
+        self.collectionViewDataSource.collectionViewModel = initialModel
+
+        let updatedModel = CollectionViewModel(
+            sectionModels: [
+                CollectionViewModel.SectionModel(
+                    cellViewModels: [],
+                    headerHeight: 44,
+                    footerHeight: 44,
+                    diffingKey: "2"
+                )
+            ]
+        )
+
+        self.collectionViewDataSource.collectionViewModel = updatedModel
+
+        XCTAssertEqual(self.mockCollectionView.callsToDeleteSections.count, 1)
+        XCTAssertEqual(
+            self.mockCollectionView.callsToDeleteSections[0],
+            IndexSet(integer: 0)
+        )
+    }
+}
+
+struct CollectionUserCellModel: CollectionViewCellViewModel, DiffableViewModel {
+
+    var accessibilityFormat: CellAccessibilityFormat = "CollectionUserCell"
+    let cellIdentifier = "CollectionUserCell"
+    let editingStyle: UITableViewCellEditingStyle = .delete
+
+    let user: User
+
+    init(user: User) {
+        self.user = user
+    }
+
+    func applyViewModelToCell(_ cell: UICollectionViewCell) -> UICollectionViewCell {
+        return cell
+    }
+
+    var diffingKey: String {
+        return self.user.uuid.uuidString
+    }
+}
+
+struct User {
+    let name: String
+    let uuid = UUID()
+}
+
+struct UserGroup {
+    let name: String
+    var users: [User]
+}
