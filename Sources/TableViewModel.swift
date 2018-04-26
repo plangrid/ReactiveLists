e8dcbbe97516a4a60fa8f1265983dded85c46f7f
Jesse Squires <jesse@jessesquires.com>
2018-04-26 14:27:38 -0700

    fix compile error

--- Sources/TableViewModel.swift
+++ Sources/TableViewModel.swift
@@ -104,7 +104,7 @@ public struct TableViewModel {
         guard indexPath.count >= 2, // In rare cases, we've seen UIKit give us a bad IndexPath
             let section = self[indexPath.section],
             let cellViewModels = section.cellViewModels, cellViewModels.count > indexPath.row else { return nil }
-        return cellViewModels[ifExists: indexPath.row]
+        return cellViewModels[indexPath.row]
     }
 
     /// Provides a description of the table view content in terms of diffing keys. These diffing keys
