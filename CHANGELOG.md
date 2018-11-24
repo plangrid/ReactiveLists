f54477db926bbca43bc24110fc81149620058da1
Arjun Nayini <aanayini@gmail.com>
2018-11-23 18:18:48 -0800

    [SwiftLint] Upgrade to 0.28.1
    Upgrade SwiftLint, add a few new rules
    and rerun the linter.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -11,6 +11,8 @@ This release closes the [0.4.0 milestone](https://github.com/plangrid/ReactiveLi
 
 ### Breaking
 
+- Upgrades SwiftLint to 0.28.1 ([#149](https://github.com/plangrid/ReactiveLists/pull/149), [@anayini](https://github.com/anayini)
+
 - Updates the initializers for `TableSectionViewModel` and `CollectionSectionViewModel` so that the `diffingKey` argument is _required_. This prevents accidental misuse of the automatic diffing API, which was possible if you relied on the previous default parameter value. ([#147](https://github.com/plangrid/ReactiveLists/pull/147), [@ronaldsmartin](https://github.com/ronaldsmartin))
 
 - Upgrades DifferenceKit to 0.7.2  ([#148](https://github.com/plangrid/ReactiveLists/pull/148), [@benasher44](https://github.com/benasher44)
--- Tests/TableView/TableViewDriverTests.swift
+++ Tests/TableView/TableViewDriverTests.swift
@@ -39,17 +39,20 @@ final class TableViewDriverTests: XCTestCase {
                 diffingKey: nil,
                 cellViewModels: [],
                 headerViewModel: TestHeaderFooterViewModel(height: 10, viewKind: .header, label: "A"),
-                footerViewModel: TestHeaderFooterViewModel(height: 11, viewKind: .footer, label: "A")),
+                footerViewModel: TestHeaderFooterViewModel(height: 11, viewKind: .footer, label: "A")
+            ),
             TableSectionViewModel(
                 diffingKey: nil,
                 cellViewModels: ["A", "B", "C"].map { _generateTestCellViewModel($0) },
                 headerViewModel: nil,
-                footerViewModel: TestHeaderFooterViewModel(title: "footer_2", height: 21)),
-             TableSectionViewModel(
+                footerViewModel: TestHeaderFooterViewModel(title: "footer_2", height: 21)
+            ),
+            TableSectionViewModel(
                 diffingKey: nil,
                 cellViewModels: ["D", "E", "F"].map { _generateTestCellViewModel($0) },
                 headerViewModel: TestHeaderFooterViewModel(title: "header_3", height: 30),
-                footerViewModel: nil),
+                footerViewModel: nil
+            )
             ], sectionIndexTitles: ["A", "Z", "Z"])
         self._tableViewDataSource = TableViewDriver(
             tableView: tableView,
